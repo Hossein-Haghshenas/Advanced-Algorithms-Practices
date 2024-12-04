9d8907372acf1c509f6e8175cf877aa90d5f1b08
Hossein <haghshenas3c@gmail.com>
2024-12-04 12:35:40 +0330

    Update dfs search functionalities

--- DFS-Search.py
+++ DFS-Search.py
@@ -1,10 +1,53 @@
-# define graph as a adjacency list
-test_graph = {
+# define test graphs as a adjacency list
+test_graph_1 = {
     0: [1, 2, 3],
     1: [0],
     2: [0, 4],
     3: [0],
     4: [2]
+}
+
+test_graph_2 = {
+    1: [2, 4],
+    2: [5],
+    3: [5, 6],
+    4: [2],
+    5: [4],
+    6: [6]
+}
+
+test_graph_3 = {
+    "s": ["r", "u", "v"],
+    "r": ["s", "t", "w"],
+    "u": ["s", "t", "y"],
+    "v": ["s", "w", "y"],
+    "t": ["u", "r"],
+    "w": ["r", "v", "x", "z"],
+    "y": ["u", "v", "x"],
+    "x": ["w", "z"],
+    "z": ["w", "x"],
+}
+
+test_graph_4 = {
+    "s": ["v"],
+    "v": ["w"],
+    "w": ["s"],
+    "q": ["s", "w", "t"],
+    "t": ["x", "y"],
+    "x": ["z"],
+    "y": ["q"],
+    "z": ["x"],
+    "r": ["u", "y"],
+    "u": ["y"],
+}
+
+test_graph_5 = {
+    0: [1, 2],
+    1: [3],
+    2: [3, 4],
+    3: [5],
+    4: [5],
+    5: []
 }
 
 
@@ -38,10 +81,22 @@ class DepthFirstSearchClass:
         return self.visited_nodes
 
 
-# initializer dfs class
-dfs_class = DepthFirstSearchClass(test_graph)
-# call dfs process function
-visited_nodes = dfs_class.dfs_process(0)
+def search_on_graph(graph, start_node, print_text):
+    # initializer dfs class
+    dfs_class = DepthFirstSearchClass(graph)
+    # call dfs process function
+    visited_nodes = dfs_class.dfs_process(start_node)
 
-# print the result
-print(f"visited nodes list is : {visited_nodes}")
+    # print the result
+    print(f"{print_text} : {visited_nodes}")
+
+
+search_on_graph(test_graph_1, 0, 'visited nodes list for graph test-1 is')
+print("-------------------------------------------------------------------")
+search_on_graph(test_graph_2, 1, 'visited nodes list for graph test-2 is')
+print("-------------------------------------------------------------------")
+search_on_graph(test_graph_3, 's', 'visited nodes list for graph test-3 is')
+print("-------------------------------------------------------------------")
+search_on_graph(test_graph_4, 's', 'visited nodes list for graph test-4 is')
+print("-------------------------------------------------------------------")
+search_on_graph(test_graph_5, 0, 'visited nodes list for graph test-5 is')
